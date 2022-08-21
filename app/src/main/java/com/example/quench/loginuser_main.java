3f8165819bb3772dc70db83957c825023d796179
dex-gajwani <104737345+dex-gajwani@users.noreply.github.com>
2022-08-21 23:26:41 +0530

    change

--- app/src/main/java/com/example/quench/loginuser_main.java
+++ app/src/main/java/com/example/quench/loginuser_main.java
@@ -2,13 +2,54 @@ package com.example.quench;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class loginuser_main extends AppCompatActivity {
+
+    VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loginpage_user);
+
+        videoView = findViewById(R.id.video_view);
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.back_orange);
+        videoView.setVideoURI(uri);
+        videoView.start();
+
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.setLooping(true);
+            }
+        });
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        videoView.resume();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        videoView.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        videoView.suspend();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        videoView.stopPlayback();
     }
 }
