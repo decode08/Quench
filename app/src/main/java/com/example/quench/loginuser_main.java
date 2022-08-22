121264cb8a212d15cbe0bec7099021294ec1f3cd
dex-gajwani <104737345+dex-gajwani@users.noreply.github.com>
2022-08-22 10:42:16 +0530

    change

--- app/src/main/java/com/example/quench/loginuser_main.java
+++ app/src/main/java/com/example/quench/loginuser_main.java
@@ -2,14 +2,19 @@ package com.example.quench;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.VideoView;
 
 public class loginuser_main extends AppCompatActivity {
 
     VideoView videoView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +30,14 @@ public class loginuser_main extends AppCompatActivity {
             @Override
             public void onPrepared(MediaPlayer mediaPlayer) {
                 mediaPlayer.setLooping(true);
+            }
+        });
+        button = findViewById(R.id.Submit);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(loginuser_main.this, loginpage_ngo.class);
+                startActivity(intent);
             }
         });
     }
