43a75471bb436ff185a9c37ae05ead7ec5b2aba5
decode08 <104908202+decode08@users.noreply.github.com>
2022-09-09 01:15:01 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/quench/Models/users.java
@@ -0,0 +1,29 @@
+package com.example.personal.Models;
+
+public class users {
+    String eMail, password;
+    public users(String eMail, String password) {
+        this.eMail = eMail;
+        this.password = password;
+    }
+//public users(){}
+//    public users(String eMail, String password) {
+//        this.eMail = eMail;
+//        this.password = password;
+//    }
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
